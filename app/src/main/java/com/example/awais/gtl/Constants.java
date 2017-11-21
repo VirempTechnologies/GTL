6d181d9da8106ac7a8cc7c03616bd2b8e6699930
awais0094 <awais00094@gmail.com>
2017-11-21 10:00:38 +0500

    this is the second commit by awais

--- app/src/main/java/com/example/awais/gtl/Constants.java
+++ app/src/main/java/com/example/awais/gtl/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static String IPAddress= "192.168.100.5:8000";
     public static String loginURL ="http://"+IPAddress+"/api/login";
     public static String getBagUrl ="http://"+IPAddress+"/api/my_bag";
-
+    public static short test = 1;
 
 
 }
